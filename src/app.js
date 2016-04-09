a67353c3e9cfcc4e8a78afa67fc716269c53b5b6
Rogoon <rogoondoespebble@gmail.com>
2016-04-08 17:58:16 -0700

    Times use 12 hour clock
    Closes #9

--- src/app.js
+++ src/app.js
@@ -308,7 +308,29 @@ function convertTime(time){
   
   var date = new Date(parseInt(time.substring(6, 19)));
 
-  return date.getHours() + ':' + date.getMinutes();
+  var hours = parseInt(date.getHours());
+  var mins = parseInt(date.getMinutes());
+  var ampm = 'am';
+  
+  switch(hours){
+    case 0:
+      hours = 12;
+      break;
+    case 12:
+      ampm = 'pm';
+      break;
+    default:
+      if (parseInt(date.getHours()) > 12){
+        hours = hours - 12;
+        ampm = 'pm';
+      }
+  }
+  
+  if(mins < 10){
+    mins = mins + '0';
+  }
+  
+  return hours + ':' + mins + ' ' + ampm;
 }
 
 // returns current date in YYYY-MM-DD format.
